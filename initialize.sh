097491179f6c6ef064f7cc449d3bb73d174eddd0
darko <darko@coulters.ca>
2019-03-28 16:19:46 -0400

    Updated template

--- initialize.sh
+++ initialize.sh
@@ -6,15 +6,15 @@
 echo "updating configuration for environment"
 
 if [ "$TARGET_ENVIRONMENT" == "DEV" ]; then
-    cp ./settings/settings_dev.py /code/inspection_reporting/main_app/main_app/settings.py
+    cp ./settings/settings_dev.py /code/project_directory/main_app/main_app/settings.py
     cp ./crontab/crontab_dev.txt ./crontab/crontab.txt
 
 elif [ "$TARGET_ENVIRONMENT" == "PROD" ]; then
-    cp ./settings/settings_prod.py /code/inspection_reporting/main_app/main_app/settings.py
+    cp ./settings/settings_prod.py /code/project_directory/main_app/main_app/settings.py
     cp ./crontab/crontab_prod.txt ./crontab/crontab.txt
 fi
 
-crontab ./crontab/crontab.txt
+#crontab ./crontab/crontab.txt
 
 #python /code/inspection_reporting/main_app/manage.py collectstatic
 #python /code/inspection_reporting/main_app/manage.py makemigrations
