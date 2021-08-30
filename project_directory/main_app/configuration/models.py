d9fb4d6a33395b604c6508234a511f51482e8ba4
Sam Helman <sam@coultersoftware.ca>
2021-08-30 16:51:11 -0700

    added configuration models

--- project_directory/main_app/configuration/models.py
+++ project_directory/main_app/configuration/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class AbstractConfig(models.Model):
+    class Meta:
+        abstract = True
+
+    code = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        "users.User", 
+        null=True, 
+        related_name="%(class)s_created_by",
+        on_delete=models.SET_NULL
+    )
+    updated_on = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(
+        "users.User", 
+        null=True,
+        related_name="%(class)s_updated_by",
+        on_delete=models.SET_NULL
+    )
+    active = models.BooleanField()
+
+class ProductConfig(AbstractConfig):
+    pass
+
+class RecipeConfig(AbstractConfig):
+    product = models.ForeignKey("configuration.ProductConfig", on_delete=models.CASCADE)
+
+class RecipeStepConfig(AbstractConfig):
+    recipe = models.ForeignKey("configuration.RecipeConfig", on_delete=models.CASCADE)
