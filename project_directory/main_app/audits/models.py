0af09642b443b25e41bae3d6e5eb1bf53a3b9969
Sam Helman <sam@coultersoftware.ca>
2021-08-30 16:20:43 -0700

    added audit models

--- project_directory/main_app/audits/models.py
+++ project_directory/main_app/audits/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    message = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey("users.User", null=True, on_delete=models.SET_NULL)
