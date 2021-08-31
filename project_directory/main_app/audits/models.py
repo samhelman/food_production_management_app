ae633dbd1406575783b0aaf4db87627d1e433922
Sam Helman <sam@coultersoftware.ca>
2021-08-30 17:13:12 -0700

    added generic fk to note model

--- project_directory/main_app/audits/models.py
+++ project_directory/main_app/audits/models.py
@@ -1,6 +1,13 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 class Note(models.Model):
     message = models.CharField(max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey("users.User", null=True, on_delete=models.SET_NULL)
+
+    # GENERIC FK
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
